74c634509d50c7d2c414fb182d514f89a606f471
Sushi-Mampfer <84100259+Sushi-Mampfer@users.noreply.github.com>
2026-02-24 23:22:19 +0100

    wav files are now handled in chunks

--- src/workflows/parser.rs
+++ src/workflows/parser.rs
@@ -1,3 +1,9 @@
+use std::io::BufReader;
+use std::io::Cursor;
+use std::io::Read;
+use std::io::Seek;
+use std::io::SeekFrom;
+
 use serde::Deserialize;
 use serde::Serialize;
 use wasm_bindgen::prelude::*;
@@ -5,11 +11,21 @@ use worker::console_error;
 use worker::console_log;
 use worker::wasm_bindgen_futures;
 use worker::Env;
+use worker::Range;
 use workflows_rs::{from_value, WorkflowEvent, WorkflowStep};
 
 #[derive(Deserialize, Clone)]
 struct Input {
     pub id: String,
+}
+
+#[derive(Serialize, Deserialize, Clone)]
+struct Headers {
+    headers: Vec<u8>,
+    offset: u64,
+    size: u64,
+    num_channels: u16,
+    bits_per_sample: u16,
 }
 
 #[derive(Serialize)]
@@ -62,57 +78,177 @@ impl ParseWorkflow {
 
         let env = self.env.clone();
         let payload = event.payload.clone();
-        let transcription = step
-            .exec("transcribe", None, move || {
+        let headers = step
+            .exec("parse wav", None, move || {
                 let env = env.clone();
                 let payload = payload.clone();
                 async move {
-                    let audio = env
+                    let header_bytes = env
                         .bucket("NOTES")
                         .map_err(|_| "Failed to get bucket.".to_string())?
                         .get(format!("raw/{}", payload.id))
+                        .range(Range::Prefix { length: 65536 })
                         .execute()
                         .await
-                        .map_err(|_| "Failed to get raw.".to_string())?
-                        .ok_or("Failed to find raw.".to_string())?
+                        .map_err(|_| "Failed to get header bytes.".to_string())?
+                        .ok_or("Failed to find header bytes.".to_string())?
                         .body()
-                        .ok_or("Failed to get raw body.".to_string())?
+                        .ok_or("Failed to get header bytes body.".to_string())?
                         .bytes()
                         .await
-                        .map_err(|_| "Failed to convert raw to bytes.".to_string())?;
-                    let ai = env
-                        .ai("AI")
-                        .map_err(|_| "Failed to get AI binding.".to_string())?;
-                    match ai
-                        .run::<WhisperInput, WhisperOutput>(
-                            "@cf/openai/whisper-large-v3-turbo",
-                            WhisperInput {
-                                audio: AudioField::Raw {
-                                    body: audio,
-                                    contentType: "audio/wav".into(),
-                                },
-                            },
-                        )
-                        .await
-                    {
-                        Ok(transcript) => Ok(transcript.text),
-                        Err(e) => {
-                            console_error!("Failed to transcribe audiofile {}.", e);
-                            Err("Failed to transcribe audiofile.".to_string())
-                        }
+                        .map_err(|_| "Failed to get header bytes as bytes.".to_string())?;
+                    let header_cursor = Cursor::new(header_bytes);
+                    let mut header = BufReader::new(header_cursor);
+
+                    let mut buf = [0u8; 4];
+                    header
+                        .read_exact(&mut buf)
+                        .map_err(|_| "Failed to get riff start.".to_string())?;
+                    if &buf != b"RIFF" {
+                        return Err("Not a riff file".to_string());
                     }
+
+                    header
+                        .seek(SeekFrom::Current(8))
+                        .map_err(|_| "Failed to move seeker.".to_string())?;
+
+                    let mut num_channels = 0;
+                    let mut bits_per_sample = 0;
+
+                    let (offset, size) = loop {
+                        let mut id = [0u8; 4];
+                        let mut size_buf = [0u8; 4];
+                        header
+                            .read_exact(&mut id)
+                            .map_err(|_| "Failed to read id.".to_string())?;
+                        header
+                            .read_exact(&mut size_buf)
+                            .map_err(|_| "Failed to read size.".to_string())?;
+                        let size = u32::from_le_bytes(size_buf) as u64;
+
+                        if &id == b"fmt " {
+                            let mut fmt = vec![0u8; size as usize];
+                            header
+                                .read_exact(&mut fmt)
+                                .map_err(|_| "Failed to get fmt chunk.".to_string())?;
+                            num_channels = u16::from_le_bytes([fmt[2], fmt[3]]);
+                            bits_per_sample = u16::from_le_bytes([fmt[14], fmt[15]]);
+                        } else if &id == b"data" {
+                            let data_offset = header
+                                .stream_position()
+                                .map_err(|_| "Failed to get offset.".to_string())?;
+                            break (data_offset, size);
+                        }
+                        header
+                            .seek(SeekFrom::Current(size as i64))
+                            .map_err(|_| "Failed to move seeker.".to_string())?;
+                    };
+
+                    let mut headers = vec![0u8; offset as usize];
+                    header
+                        .seek(SeekFrom::Start(0))
+                        .map_err(|_| "Failed to move seeker.".to_string())?;
+                    header
+                        .read_exact(&mut headers)
+                        .map_err(|_| "Failed to read headers.".to_string())?;
+
+                    Ok(Headers {
+                        headers,
+                        offset,
+                        size,
+                        num_channels,
+                        bits_per_sample,
+                    })
                 }
             })
             .await;
-        let transcript = match transcription {
+
+        let headers = match headers {
             Ok(t) => t,
-            Err(e) => {
-                return Err(JsValue::from_str(&format!(
-                    "Failed to transcribe audiofile: {}",
-                    e
-                )))
-            }
+            Err(e) => return Err(JsValue::from_str(&format!("Failed to get headers: {}", e))),
         };
+
+        let frame_size = (headers.bits_per_sample as u64 / 8) * headers.num_channels as u64;
+        let audio_per_chunk = (29 * 1024 * 1024 - headers.offset) / frame_size * frame_size;
+        let num_chunks = headers.size.div_ceil(audio_per_chunk);
+        let mut transcript = String::new();
+
+        for i in 0..num_chunks {
+            let bytes_to_read = audio_per_chunk.min(headers.size - i * audio_per_chunk);
+            let offset = headers.offset;
+            let header = headers.headers.clone();
+
+            let env = self.env.clone();
+            let payload = event.payload.clone();
+            let transcription = step
+                .exec("transcribe", None, move || {
+                    let env = env.clone();
+                    let payload = payload.clone();
+                    let mut header = header.clone();
+                    async move {
+                        let mut audio = env
+                            .bucket("NOTES")
+                            .map_err(|_| "Failed to get bucket.".to_string())?
+                            .get(format!("raw/{}", payload.id))
+                            .range(Range::OffsetWithLength {
+                                offset: offset + i * audio_per_chunk,
+                                length: bytes_to_read,
+                            })
+                            .execute()
+                            .await
+                            .map_err(|_| "Failed to get raw.".to_string())?
+                            .ok_or("Failed to find raw.".to_string())?
+                            .body()
+                            .ok_or("Failed to get raw body.".to_string())?
+                            .bytes()
+                            .await
+                            .map_err(|_| "Failed to convert raw to bytes.".to_string())?;
+
+                        let riff_size = (4 + (offset as usize - 8) + 8 + audio.len()) as u32;
+                        header[4..8].copy_from_slice(&riff_size.to_le_bytes());
+
+                        let data_size_offset = offset as usize - 4;
+                        header[data_size_offset..offset as usize]
+                            .copy_from_slice(&(audio.len() as u32).to_le_bytes());
+
+                        header.append(&mut audio);
+
+                        let ai = env
+                            .ai("AI")
+                            .map_err(|_| "Failed to get AI binding.".to_string())?;
+                        match ai
+                            .run::<WhisperInput, WhisperOutput>(
+                                "@cf/openai/whisper-large-v3-turbo",
+                                WhisperInput {
+                                    audio: AudioField::Raw {
+                                        body: header,
+                                        contentType: "audio/wav".into(),
+                                    },
+                                },
+                            )
+                            .await
+                        {
+                            Ok(transcript) => Ok(transcript.text),
+                            Err(e) => {
+                                console_error!("Failed to transcribe audiofile {}.", e);
+                                Err("Failed to transcribe audiofile.".to_string())
+                            }
+                        }
+                    }
+                })
+                .await;
+            let tmp = match transcription {
+                Ok(t) => t,
+                Err(e) => {
+                    return Err(JsValue::from_str(&format!(
+                        "Failed to transcribe audiofile: {}",
+                        e
+                    )))
+                }
+            };
+            transcript.push_str(&tmp);
+        }
+
         let env = self.env.clone();
         let cleaning = step
             .exec("cleanup", None, move || {
@@ -124,7 +260,7 @@ impl ParseWorkflow {
                         .map_err(|_| "Failed to get AI binding.".to_string())?;
                     match ai
                         .run::<LlamaInput, LlamaOutput>(
-                            "@cf/facebook/bart-large-cnn",
+                            "@cf/meta/llama-4-scout-17b-16e-instruct",
                             LlamaInput { messages: vec![LlamaMessage { role: "system".to_string(), content: "You are a teaching assistant.\n\nYour job is to clean up the text given to you.\nAfter you're done it should be one coherent lecture.\n\nRemove filler words and repetitions\nFix punctuation and capitalization\nMerge broken sentences\n\nDo NOT remove any content or meaning!\nDo NOT summarize.\nDo NOT add anything from your knowledge that wasn explicitly said.".to_string() }, LlamaMessage { role: "user".to_string(), content: transcript }] },
                         )
                         .await
@@ -151,7 +287,7 @@ impl ParseWorkflow {
         let payload = event.payload.clone();
         let cleaned_pass = cleaned.clone();
         let _ = step
-            .exec("upload_cleaned", None, move || {
+            .exec("upload cleaned", None, move || {
                 let env = env.clone();
                 let cleaned = cleaned_pass.clone();
                 let payload = payload.clone();
@@ -177,7 +313,7 @@ impl ParseWorkflow {
                         .map_err(|_| "Failed to get AI binding.".to_string())?;
                     match ai
                         .run::<LlamaInput, LlamaOutput>(
-                            "@cf/facebook/bart-large-cnn",
+                            "@cf/meta/llama-4-scout-17b-16e-instruct",
                             LlamaInput { messages: vec![LlamaMessage { role: "system".to_string(), content: "You are a study assistant.\n\nYou create text files that explain the content of the lession you get as an input.\nIf there is any knowledge needed to understand the lession you take it as granted and don't explain it.\n\nYou HAVE to include everything asked for.\nYou ONLY answer in raw text without any styling except for line breakes.\nYou do NOT remove any content or create new content.\nYou add single choice questions at the end with the answers written in flipped askii. It can be however many question you find adequate, but it should only be basic understanding questions.".to_string() }, LlamaMessage { role: "user".to_string(), content: cleaned }] },
                         )
                         .await
@@ -203,7 +339,7 @@ impl ParseWorkflow {
         let env = self.env.clone();
         let payload = event.payload.clone();
         let _ = step
-            .exec("upload_cleaned", None, move || {
+            .exec("upload summary", None, move || {
                 let env = env.clone();
                 let summary = summary.clone();
                 let payload = payload.clone();
